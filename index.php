d3ffcac74251fcb17a1fb6e34998a6d6e42eee8a
Asad <mdasadask@gmail.com>
2024-06-25 22:56:47 +0600

    set direct url and test facebook folder in outside

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/secret.php';
@@ -7,16 +8,32 @@ use Facebook\Facebook;
 use Facebook\Exceptions\FacebookResponseException;
 use Facebook\Exceptions\FacebookSDKException;
 
-// echo "app_secret= " . app_secret . PHP_EOL;
-// die();
-
 $fb = new Facebook([
-    'app_id' => app_id,
-    'app_secret' => app_secret,
-    'default_graph_version' => 'v20.0',
+    'app_id' => APP_ID,
+    'app_secret' => APP_SECRET,
+    'default_graph_version' => DEFAULT_GRAPH_VERSION,
 ]);
 
+$permissions = ['email', 'pages_manage_engagement', 'pages_manage_posts', 'pages_read_engagement', 'pages_read_user_content', 'pages_show_list', 'pages_manage_metadata'];
+
 $helper = $fb->getRedirectLoginHelper();
+
+$login_url = $helper->getLoginUrl(REDIRECT_URL, $permissions);
+
+var_dump($login_url);
+die();
+
+// require_once 'Facebook/autoload.php';
+// use Facebook\Facebook;
+// use Facebook\Exceptions\FacebookResponseException;
+// use Facebook\Exceptions\FacebookSDKException;
+// $fb = new Facebook(array(
+//     'app_id' => FB_APP_ID,
+//     'app_secret' => FB_APP_SECRET,
+//     'default_graph_version' => META_GRAPH_API_VERSION,
+// ));
+// $helper = $fb->getRedirectLoginHelper();
+
 
 try {
     $accessToken = $helper->getAccessToken();
